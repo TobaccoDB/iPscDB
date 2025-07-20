1ae9812eda5a127bd84236da4d33c3b63bbf8edf
guozeqing_zz <guoqz@chinatelecom>
2025-07-20 11:19:44 +0800

    delete code

--- backend/service-analysis/celery_start.sh
+++
@@ -1,4 +0,0 @@
-# 启动 celery_cellranger 队列
-celery multi start worker -A django_server -l info -f logs/celery_cellranger.log --pidfile=pids/celery_cellranger.pid -n cellranger_worker -Q cellranger
-# 启动 默认队列
-celery multi start worker -A django_server -l info -f logs/celery_default.log --pidfile=pids/celery_default.pid
--- backend/service-api/plant_marker/apps/plant_home/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlantHomeConfig(AppConfig):
-    name = 'plant_home'
